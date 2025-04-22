e17788c84836136d50e4e239652399dda6c2f7f8
MilanBlairon <r0981022@student.vives.be>
2025-04-22 11:25:18 +0200

    Initiële commit van Drupal met alle bestanden en een gitiginore

---
+++ drupal/sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'milan',
+  'password' => 'vives',
+  'prefix' => '',
+  'host' => 'drupal-db',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'fpk-hNbkUWkXawnqmeyt90UR5HDRuMKkRK1sQwDIpsKaLReMEIiWlFSft-FnBmL6aKpjcYGRTA';
+$settings['config_sync_directory'] = 'sites/default/files/config_3eLRaaLXXVZxUvEPYwn2Cn3vrYXKTutPBucQYUydZTr7jztWwsqxDEN3z-qs6IaJBKceOClXPA/sync';
